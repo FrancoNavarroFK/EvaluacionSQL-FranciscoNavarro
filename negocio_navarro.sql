2be7fbda2e0fd263b01e32777e6e038bb3b391d1
krakenav9 <francisconavarroflores@gmail.com>
2022-11-13 18:17:30 -0300

    version 0.98 de la prueba

--- negocio_navarro.sql
+++ negocio_navarro.sql
@@ -87,11 +87,12 @@ SELECT * FROM negocio_navarro.efectivoodigital;
 
 INSERT INTO boletacarrocompra (fecha,nombre,efectivoodigital_id)
 -- aquí añadimos una compra realizada en 2021 para poder ocupar el llamado de datos con filtro de fecha BETWEEN
-VALUES (20221111, 'compra2cocas', 3 ), ('20220101', '5cocas', 1 ), ('20211229', '1coca', 2 );
+VALUES (20221111, 'compra2cocas', 3 ), ('20220101', '5cocas', 1 ), ('20211229', '1coca', 2 ), (20221211, 'compra8cocas3L', 3 );
 SELECT * FROM negocio_navarro.boletacarrocompra;
 
+
 INSERT INTO detallecantidad (boletacarrocompra_id, producto_id, cantidad, precio)
-  VALUES (1, 1, 2, 2500), (1, 3, 1, 3000), (2,2,5,1500), (3,1,1,2500);
+  VALUES (1, 1, 2, 2500), (1, 3, 1, 3000), (2,2,5,1500), (3,1,1,2500), (4,1,8,2600);
   SELECT * FROM negocio_navarro.detallecantidad;
   
 
@@ -107,14 +108,48 @@ INSERT INTO proveedores (nombreProveedor, nombrePersonaProveedor, teléfono, cor
   -- llamar datos con JOIN
   -- Aquí ocurre un problema de duplicación de datos porque un producto tiene 2 proveedores --
   -- una solución sería que en detalle boleta también se inserte la FK del proveedor 
+  -- otra solución es que sean productos distintos, coca de 1.5L-feria, y coca 1.5L-cocacompany
   SELECT detallecantidad.boletaCarroCompra_id, detallecantidad_id, fecha, nombreProducto, cantidad, detallecantidad.precio, precioProveedor
     FROM detallecantidad CROSS JOIN producto ON (detallecantidad.producto_id=producto.producto_id)
   CROSS JOIN proveedores ON (producto.producto_id=proveedores.producto_id)
   CROSS JOIN boletacarrocompra ON (detallecantidad.boletaCarroCompra_id=boletacarrocompra.boletaCarroCompra_id);
   
-  -- omitiremos un proveedor, suponiendo que con feria ya no tenemos contacto para las cocas de 1.5litros
-  -- otra solución es que sean productos distintos, coca de 1.5L-feria, y coca 1.5L-cocacompany
-  -- queremos ver las cantidad de ventas (COUNT) de cocacola en 2022, y las ganancias (SUMA)
+  -- omitiremos el problema de los proveedores, solo sumando las ventas de cocas de 3litros que tiene un solo proveedor
+  -- queremos ver las cantidad de ventas (COUNT) de cocacola 3L en 2022, y las ganancias (SUMA)
   
+  #para contar cuantos resultados trae mi consulta, haremos SUM, porque COUNT me trae 2 compras, pero se compraron 3 bebidas (1 compra tiene 2 bebidas)
+  # --------------- tabla de cocacolas3L compradas en 2022 --------------------
+  SELECT detallecantidad.boletaCarroCompra_id, detallecantidad_id, fecha, nombreProducto, cantidad, detallecantidad.precio, precioProveedor
+    FROM detallecantidad CROSS JOIN producto ON (detallecantidad.producto_id=producto.producto_id)
+  CROSS JOIN proveedores ON (producto.producto_id=proveedores.producto_id)
+  CROSS JOIN boletacarrocompra ON (detallecantidad.boletaCarroCompra_id=boletacarrocompra.boletaCarroCompra_id)
+  #aquí aplicamos filtro de solo el año 2022
+  WHERE 
+  (producto.producto_id = 1)
+  AND (boletacarrocompra.fecha BETWEEN '20220101' AND '20221231');
+  # --------------------------------------------------------------------------------
+  # --------------------------------------------------------------------------------
   
+  # -------------- Sumamos todas las cocas3L vendidas en 2022 con SUM ------------------
+  SELECT nombreProducto, detallecantidad.precio, precioProveedor, sum(detallecantidad.cantidad), count(detallecantidad.cantidad)
+    FROM detallecantidad CROSS JOIN producto ON (detallecantidad.producto_id=producto.producto_id)
+  CROSS JOIN proveedores ON (producto.producto_id=proveedores.producto_id)
+  CROSS JOIN boletacarrocompra ON (detallecantidad.boletaCarroCompra_id=boletacarrocompra.boletaCarroCompra_id)
+  #aquí aplicamos filtro de solo el año 2022
+  WHERE 
+  (producto.producto_id = 1)
+  AND (boletacarrocompra.fecha BETWEEN '20220101' AND '20221231');
+  
+  # --------------- Calcular ganancias ----------------------
+  # restamos precio - precioProveedor, y multiplicamos por SUM
+  SELECT nombreProducto, detallecantidad.precio, precioProveedor, sum(detallecantidad.cantidad), sum((detallecantidad.precio-precioProveedor))
+    FROM detallecantidad CROSS JOIN producto ON (detallecantidad.producto_id=producto.producto_id)
+  CROSS JOIN proveedores ON (producto.producto_id=proveedores.producto_id)
+  CROSS JOIN boletacarrocompra ON (detallecantidad.boletaCarroCompra_id=boletacarrocompra.boletaCarroCompra_id)
+  #aquí aplicamos filtro de solo el año 2022
+  WHERE 
+  (producto.producto_id = 1)
+  AND (boletacarrocompra.fecha BETWEEN '20220101' AND '20221231');
+  
+
   
